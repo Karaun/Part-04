3020746e72115b08e790ff76e2dabc6ea7bd4e6e
Char.ang <114490290+Karaun@users.noreply.github.com>
2022-10-18 19:28:34 +0800

    Add files via upload

---
+++ 时间系统转换/时间系统转换/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 时间系统转换
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 打开ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "请选择你要打开的文件";
+            ofd.Filter = "txt|*.txt|All|*.*";
+            ofd.ShowDialog();
+
+            string path = ofd.FileName;
+
+            if (path == "")
+            {
+                return;
+            }
+
+            using (FileStream FsRead = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Read))
+            {
+                byte[] buffer = new byte[1024 * 1024 * 5];
+                int r = FsRead.Read(buffer, 0, buffer.Length);
+                txtSource.Text = Encoding.UTF8.GetString(buffer, 0, r);
+
+
+            }
+        }
+
+        private void 导出结果ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "请选择保存路径";
+            sfd.Filter = "tx|*.txt|All|*.*";
+            sfd.ShowDialog();
+
+            string outcome = sfd.FileName;
+            if (outcome == "")
+            {
+                return;
+            }
+            using (FileStream FsWrite = new FileStream(outcome, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                byte[] buffer = Encoding.UTF8.GetBytes(txtResult.Text);
+                FsWrite.Write(buffer, 0, buffer.Length);
+
+            }
+        }
+
+        private void 计算ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           List<Time> Time_list = new List<Time>();//时间列表
+
+            //存储路径数据
+           Time_list = Time.GetTime1(txtSource.Text);
+
+            List<string> JD_Time = new List<string>();//存储JD
+            for (int i = 0; i < Time_list.Count; i++)
+            {
+                JD_Time.Add(Time.Jd(Time_list[i]).ToString("#0.00000"));
+            }
+
+
+            List<string> Date_list = new List<string>();//存储公历
+            for (int i = 0; i < JD_Time.Count; i++)
+            {
+                string date = Time.Gregorian_calendar(Convert.ToDouble(JD_Time[i]));
+                date = date + " " + Time.Time_Format(Time_list[i]);
+                Date_list.Add(date);
+                //MessageBox.Show(Date_list[i]);
+            }
+
+            List<string> Day_count = new List<string>();//存储年积日
+            for (int i = 0; i < Time_list.Count; i++)
+            {
+                Day_count.Add(Time.day_count(Time_list[i]));
+                
+            }
+
+
+            List<string> State_list = new List<string>();//存储状态数据
+            for (int i = 0; i < Time_list.Count; i++)
+            {
+                State_list.Add(Time.State(Time_list[i]));
+            }
+
+            string result = null;
+            string t1 = "------------JD------------";
+            string t2 = "------------公历(年 月 日 时：分：秒)------------";
+            string t3 = "------------年积日------------";
+            string t4 = "------------三天打鱼两天晒网------------";
+
+            result = t1 + "\r\n" + display(JD_Time) + "\r\n" + t2 + "\r\n" + display(Date_list) + "\r\n" + t3 + "\r\n" + display(Day_count) + "\r\n" + t4 + "\r\n" + display(State_list);
+            txtResult.Text = result;
+        }
+
+
+        public string display(List<string> list)
+        {
+            string result = null;
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (i != list.Count - 1)
+                {
+                    result += list[i] + "\r\n";
+                }
+                else
+                {
+                    result += list[i];
+                }
+            }
+
+            return result;
+        }
+
+        private void 文件ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ 时间系统转换/时间系统转换/Time.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 时间系统转换
+{
+    class Time
+    {
+        #region 时间类
+        public Time(double i, double j, double k, double l, double z, double x)
+        {
+            year = i;
+            month = j;
+            day = k;
+            hour = l;
+            minute = z;
+            second = x;
+        }
+
+        private double Year;
+        private double Month;
+        private double Day;
+        private double Hour;
+        private double Minute;
+        private double Second;
+
+        public double year
+        {
+            set { Year = value; }
+            get { return Year; }
+        }
+
+        public double month
+        {
+            set { Month = value; }
+            get { return Month; }
+        }
+        public double day
+        {
+            set { Day = value; }
+            get { return Day; }
+        }
+        public double hour
+        {
+            set { Hour = value; }
+            get { return Hour; }
+        }
+        public double minute
+        {
+            set { Minute = value; }
+            get { return Minute; }
+        }
+        public double second
+        {
+            set { Second = value; }
+            get { return Second; }
+        }
+        #endregion
+
+
+        public static double Jd(Time t)//公历转儒略日(JD)
+        {
+            double JD = 1721013.5 + 367 * t.year;
+            double temp = Math.Floor((t.month + 9.0) / 12.0);
+            JD -= Math.Floor(7.0 / 4.0 * (t.year + temp));
+            JD += Math.Floor(275.0 * t.month / 9.0);
+            JD += t.day + t.hour / 24.0 + t.minute / 1440.0 + t.second / 86400.0;
+            return JD;
+
+        }
+        //Math.Floor为向下取整
+        //Math.Round：根据四舍五入取整
+        //Math.Ceiling：向上取整，有小数，整数加1
+
+
+        public static string Gregorian_calendar(double jd)//儒略日转公历
+        {
+            double a = Math.Floor(jd + 0.5);
+            double b = a + 1537.0;
+            double c = Math.Floor((b - 122.1) / 365.25);
+            double d = Math.Floor(365.25 * c);
+            double e = Math.Floor((b - d) / 30.600);
+            string day = null;
+            string month = null;
+
+
+            day = (b - d - Math.Floor(30.6001 * e) + 1 / (jd + 0.5)).ToString("#00");
+            if (e < 14)
+            {
+                e--;
+            }
+            else if (e == 14.0 || e == 15.0)
+            {
+                e -= 13;
+            }
+            month = (Math.Floor(e - 12 * Math.Floor(e / 14.0))).ToString("#00");
+            string year = (c - 4715.0 - Math.Floor((7 + Convert.ToDouble(month)) / 10.0)).ToString("#0");
+
+            string date = year + " " + month + " " + day;
+            return date;
+        }
+
+        public static string Time_Format(Time t)//时间(小时：分钟：秒)显示
+        {
+            string hour = null;
+            string minute = null;
+            string second = null;
+
+            hour = t.hour.ToString("#00");
+            minute = t.minute.ToString("#00");
+            second = t.second.ToString("#00.000000");
+
+
+
+            string tt = hour + ":" + minute + ":" + second;
+            return tt;
+        }
+
+
+        public static string day_count(Time t)//年积日
+        {
+            string day_count = null;
+            int[] tab = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            if ((t.year % 4 == 0 && t.year % 100 != 0) || (t.year % 400 == 0))
+            {
+                tab[1] = 29;
+            }
+
+            int count = 0;
+            for (int i = 0; i < Convert.ToInt32(t.month) - 1; i++)
+            {
+                count += tab[i];
+            }
+
+            count += Convert.ToInt32(t.day);
+
+            day_count = count.ToString();
+            return day_count;
+        }
+
+        //时间状态
+        public static string State(Time t)
+        {
+            string temp = null;
+            double jd = Jd(t);
+            int day = Convert.ToInt32(day_count(t));
+            int i = day % 5;
+            if (i < 3)
+            {
+                temp = "打鱼日";
+            }
+            else if (i >= 3 && i <= 5)
+            {
+                temp = "晒网日";
+            }
+
+            string state = Gregorian_calendar(jd) + "," + temp;
+
+            return state;
+
+        }
+
+
+        //通过符号分割时间
+        public static List<Time> GetTime1(string text)
+        {
+            string line;//读取行数
+            string[] str = text.Split('\n');//存储每行数据
+            string[] temp = new string[1000000];//创建集合存储起点，终点，距离数据
+            List<Time> Time_list = new List<Time>();//时间列表
+
+            //存储时间数据
+            for (int x = 0; x < str.Length; x++)
+            {
+                line = str[x];
+                temp = line.Split(' ');
+                string Y = temp[0];
+                string M = temp[1];
+                string D = temp[2];
+                string H = temp[3];
+                string U = temp[4];
+                string S = temp[5];
+                //MessageBox.Show(Y + M + D + H + U + S);
+
+                Time tt = new Time(Convert.ToDouble(Y), Convert.ToDouble(M), Convert.ToDouble(D), Convert.ToDouble(H), Convert.ToDouble(U), Convert.ToDouble(S));
+                Time_list.Add(tt);
+
+            }
+            return Time_list;
+        }
+
+
+        //输入时间列表进行切割
+        public static List<Time> GetTime2(List<string> time)
+        {
+            List<Time> Time_list = new List<Time>();//时间列表
+
+            //获取数据继续分割
+            for (int i = 0; i < time.Count; i++)
+            {
+
+                double Y = Convert.ToDouble(time[i].Substring(0, 4));
+                double M = Convert.ToDouble(time[i].Substring(4, 2));
+                double D = Convert.ToDouble(time[i].Substring(6, 2));
+                double H = Convert.ToDouble(time[i].Substring(8, 2));
+                double U = Convert.ToDouble(time[i].Substring(10, 2));
+                double S = Convert.ToDouble(time[i].Substring(12, 2));
+                //MessageBox.Show(Y + M + D + H + U + S);
+
+                Time tt = new Time(Convert.ToDouble(Y), Convert.ToDouble(M), Convert.ToDouble(D), Convert.ToDouble(H), Convert.ToDouble(U), Convert.ToDouble(S));
+                Time_list.Add(tt);
+
+            }
+            return Time_list;
+        }
+
+    }
+}
